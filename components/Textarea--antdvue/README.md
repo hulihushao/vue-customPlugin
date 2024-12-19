b90193f316c352816e3b48afdc5866764e11b6c7
taojianping <taojp@jurassic.com.cn>
2024-12-19 10:26:11 +0800

    新增文本域组件

---
+++ components/Textarea--antdvue/README.md
@@ -0,0 +1,9 @@
+## 说明
+
+封装 antdVue 的文本域输入字数提示。
+
+## 使用
+
+新增  props:{
+  maxLength: Number, 无默认值
+}
---
+++ components/Textarea--antdvue/t-textarea.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="t-textarea" style="position: relative">
+    <a-textarea
+      ref="textarea"
+      :class="{ 'new-padding-left': $attrs.maxLength }"
+      v-bind="$attrs"
+      v-model="textareaValue"
+    />
+    <span
+      v-if="$attrs.maxLength"
+      style="
+        position: absolute;
+        user-select: none;
+        z-index: 9;
+        bottom: 24px;
+        font-weight: 400;
+        font-size: 12px;
+        color: #86909c;
+        line-height: 12px;
+        transition: unset;
+      "
+      :style="{ left: `calc(${textareaWidth}px - ${(textareaValue || '1').length.toString().length * 7.5}px - 38px)` }"
+    >
+      {{ textareaValue ? textareaValue.length : 0 }}/{{ $attrs.maxLength }}
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  name: 't-textarea',
+  data() {
+    return {
+      textareaValue: undefined,
+      textareaWidth: 0
+    }
+  },
+  props: {
+    value: {
+      type: [String, Number],
+      default: ''
+    }
+  },
+  watch: {
+    textareaValue(val) {
+      this.$emit('input', val)
+    },
+    value(val) {
+      this.textareaValue = val
+    }
+  },
+  created() {},
+  mounted() {
+    this.textareaWidth = this.$refs.textarea.$el.firstChild.offsetWidth
+  },
+  methods: {}
+}
+</script>
+<style lang="less" scoped>
+.new-padding-left {
+  /deep/textarea.ant-input {
+    padding-right: 45px !important;
+  }
+}
+</style>
