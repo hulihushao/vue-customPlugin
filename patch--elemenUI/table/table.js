ea8c9d25e5ac526f7eb6ff488f2451f3faa41187
taojianping <taojp@jurassic.com.cn>
2025-03-28 15:24:12 +0800

    优化elementUI table拖拽排序

--- patch--elemenUI/table/table.js
+++ patch--elemenUI/table/table.js
@@ -20,13 +20,13 @@ export default {
           draggingIndex: null, // 记录当前拖拽行的索引
           tableData2: [],
           draggingLevel: 0,
-          draggingParent: null,
-          rowBgColors: []
+          hoverDom: null
         }
       },
       watch: {
         data: {
           handler(val) {
+            if (!val) return
             if (this.drag) {
               this.tableData2 = this.expendAll(this.data, [])
               // console.log(this.tableData2)
@@ -44,11 +44,6 @@ export default {
               const allrow = this.$el.querySelectorAll('tbody tr')
               // console.log(allrow)
               allrow.forEach((row, index) => {
-                row.style.transition = 'all 0.3s'
-                if (!row.className.includes('el-table__row--level-')) {
-                  row.classList.add('el-table__row--level-0')
-                }
-                this.rowBgColors.push(window.getComputedStyle(row).backgroundColor)
                 row.draggable = true // 设置行元素为可拖拽
                 // 开始拖拽时的事件处理
                 row.ondragstart = (event) => this.handleDragStart(event, index)
@@ -56,24 +51,23 @@ export default {
                 row.ondragover = (event) => this.handleDragOver(event)
                 // 拖拽放下时的事件处理
                 row.ondrop = (event) => this.handleDrop(event, index)
-                row.ondragend = () => {
-                  allrow.forEach((row, index) => {
-                    row.style.backgroundColor = this.rowBgColors[index]
-                  })
-                }
               })
-            })
+            }, 500)
           })
         },
         // 处理拖拽开始事件
         handleDragStart(event, index) {
           this.draggingIndex = index // 记录当前拖拽行的索引
-          this.draggingLevel =
-            event.target.className.split(' ').find((item) => item.includes('el-table__row--level-')) ||
-            'el-table__row--level-0'
-          this.draggingParent = this.getTargetParent(event.target)
+          this.draggingLevel = event.target.className.split(' ')[1] || 'el-table__row--level-0'
+          if (
+            this.draggingLevel !== 'el-table__row--level-0' &&
+            !event.target.className.includes('el-table__row--level-')
+          ) {
+            this.draggingLevel = 'el-table__row--level-0'
+          }
           event.dataTransfer.effectAllowed = 'move' // 设置拖拽效果为移动
           event.dataTransfer.setData('text/plain', index) // 将索引存储到拖拽数据中
+          // console.log(this.draggingLevel)
           this.drag.dragstart && this.drag.dragstart(this.tableData2[this.draggingIndex], event)
         },
         // 处理拖拽经过事件
@@ -84,34 +78,60 @@ export default {
             target = target.parentNode
           }
           // 如果目标索引和拖拽层级不同，不允许放置
-          if (this.isDropable(target)) {
+          if (
+            !target ||
+            (this.draggingLevel.includes('level-0') && !target.className.split(' ')[1]) ||
+            target.className.split(' ')[1] === this.draggingLevel
+          ) {
             event.preventDefault() // 阻止默认事件，允许放置
-            this.$el.querySelectorAll('tbody tr').forEach((item, index) => {
-              item.style.backgroundColor = this.rowBgColors[index]
+            if (this.hoverDom) {
+              this.hoverDom.childNodes.forEach((item) => {
+                item.style.borderBottom = '' // 设置拖拽经过行的边框
+              })
+            }
+            this.hoverDom = target
+            target.childNodes.forEach((item) => {
+              item.style.borderBottom = `2px solid ${this.drag.hoverBorderColor || '#409EFF'}` // 设置拖拽经过行的边框
             })
-            target.style.backgroundColor = '#409eff1a'
-            event.dataTransfer.dropEffect = 'move' // 设置拖拽效果为移动
+          } else {
+            if (this.hoverDom) {
+              this.hoverDom.childNodes.forEach((item) => {
+                item.style.borderBottom = '' // 设置拖拽经过行的边框
+              })
+            }
           }
+          event.dataTransfer.dropEffect = 'move' // 设置拖拽效果为移动
           this.drag.dragover && this.drag.dragover(this.tableData2[this.draggingIndex], event)
         },
         // 处理拖拽放下事件
         handleDrop(event, targetIndex) {
+          if (this.hoverDom) {
+            this.hoverDom.childNodes.forEach((item) => {
+              item.style.borderBottom = '' // 设置拖拽经过行的边框
+            })
+            this.hoverDom = null
+          }
           const sourceIndex = this.draggingIndex // 获取开始拖拽时记录的索引
           const sourceData = this.tableData2[sourceIndex]
           const targetData = this.tableData2[targetIndex]
           // 如果源索引和目标索引相同，直接返回
           if (sourceIndex === targetIndex) return
           let target = event.target
-          // console.log(target)
+          console.log(target)
           while (target && target.nodeName !== 'TR') {
             target = target.parentNode
           }
           // 如果目标索引和拖拽层级不同，直接返回
-          if (this.isDropable(target)) {
+          if (
+            !target ||
+            (this.draggingLevel.includes('level-0') && !target.className.split(' ')[1]) ||
+            target.className.split(' ')[1] === this.draggingLevel
+          ) {
           } else {
             return
           }
           this.draggingIndex = null // 重置拖拽索引
+          target = null
           this.drag?.dragend &&
             this.drag.dragend({ data: sourceData, index: sourceIndex }, { data: targetData, index: targetIndex }, event)
         },
@@ -123,43 +143,6 @@ export default {
             }
           })
           return list
-        },
-        getTargetParent(target) {
-          let patent = null
-          if (
-            !target.className.includes('el-table__row--level-') ||
-            target.className.includes('el-table__row--level-0')
-          ) {
-            return null
-          }
-          patent = target
-          while (patent.className === patent.previousElementSibling?.className) {
-            patent = patent.previousElementSibling
-          }
-          return patent
-        },
-        isDropable(target) {
-          const parentEl = this.getTargetParent(target)
-          let isSameParent = false
-          if (parentEl) {
-            if (parentEl === this.draggingParent) {
-              isSameParent = true
-            } else {
-              isSameParent = false
-            }
-          } else if (!parentEl && !this.draggingParent) {
-            isSameParent = true
-          }
-          if (
-            isSameParent &&
-            (!target ||
-              (this.draggingLevel.includes('level-0') && !target.className.split(' ')[1]) ||
-              (this.draggingLevel.includes('level-0') &&
-                target.className.split(' ')[1].includes('el-table__row--level-0')) ||
-              target.className.split(' ')[1] === this.draggingLevel)
-          ) {
-            return true
-          }
         }
       }
     })
